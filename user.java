ea34378415ea8dbec4c6dbfe71c12e579b5928bc
hushijap-21 <hushijap@gmail.com>
2026-01-31 15:30:33 +0530

    Add files via upload

--- user.java
+++ user.java
@@ -3,7 +3,15 @@ public class user {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int age=sc.nextInt();
+        double a=sc.nextDouble();
+        float b=sc.nextFloat();
+        String str1=sc.next();
+        String str2=sc.nextLine();
         System.out.println(age);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(str1);
+        System.out.println(str2);
     }
     
 }
